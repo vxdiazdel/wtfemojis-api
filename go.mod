ab28329fb1a919a42c983e09656f9aa2a396f9cd
Victor Diaz de Leon <vrdiazdel@gmail.com>
2025-01-29 17:57:26 -0800

    Update CORS config

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/vxdiazdel/wtfemojis-api
 go 1.22.5
 
 require (
-	github.com/gin-contrib/cors v1.7.3
 	github.com/gin-gonic/gin v1.10.0
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.7.2
--- internal/router/router.go
+++ internal/router/router.go
@@ -2,9 +2,7 @@ package router
 
 import (
 	"context"
-	"time"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/vxdiazdel/wtfemojis-api/handlers"
 	"github.com/vxdiazdel/wtfemojis-api/internal/db/stores"
@@ -15,16 +13,6 @@ func NewRouter(
 	store stores.IStore,
 ) *gin.Engine {
 	r := gin.Default()
-
-	// cors
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "https://wtfemojis.vercel.app"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Content-Type", "Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           24 * time.Hour,
-	}))
 
 	h := handlers.NewHandlerContext(
 		ctx,
